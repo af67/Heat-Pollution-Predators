6fa97115fcf273d6db00779bcc179daad243deb3
manalmbts <manal.moubtassim11@gmail.com>
2023-12-21 23:27:13 +0100

    many changes

--- dsfba_project/scripts/setup.R
+++ dsfba_project/scripts/setup.R
@@ -33,15 +33,10 @@ knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
   # cache = TRUE,
-  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
-  dpi = 300,
-  out.width = "70%",
-  fig.align = "center",
-  fig.width = 6,
-  fig.asp = 0.618,
+  fig.asp = NULL,
   fig.show = "hold",
-  message = FALSE,
-  echo = FALSE,
+  message = TRUE,
+  echo = TRUE,
   warning = FALSE
 )
 
