0ceb315671da724f3c0614576b7ffd074649417a
manalmbts <manal.moubtassim11@gmail.com>
2023-12-22 15:55:30 +0100

    eda

--- dsfba_project/scripts/setup.R
+++ dsfba_project/scripts/setup.R
@@ -9,7 +9,7 @@ packages <- c(
   "scales", # for plotting
   "knitr", "readxl", "dplyr", "tidyr", "stringr", "countrycode", "ggplot2", "plotly",
   "stargazer", "gtsummary", "corrplot", "gridExtra", "viridisLite", "htmltools",
-  "RColorBrewer", "car", "DT", "htmlwidgets", "leaflet")
+  "RColorBrewer", "car", "DT", "htmlwidgets", "leaflet", "gganimate")
 purrr::walk(packages, library, character.only = TRUE)
 
 ######################################################
